facfd742812ac155df7eb65d3bd786525f5de989
Damien DUPORTAL <damien.duportal@worldline.com>
2014-04-23 17:58:09 +0200

    [wip] Adding bats install, a basic bats test and implementing box tests on bat script (yeah crappy)

--- scripts/batch/3-run-tests.bat
+++ scripts/batch/3-run-tests.bat
@@ -1,20 +1,63 @@
 
-REM Cleaning tmp dir
 @echo off
 if exist tmp (
+	REM Cleaning old sanbox
+	@echo off
 	cd tmp
+	@echo off
 	vagrant destroy -f
+	@echo off
 	cd ..
+	@echo off
 	rmdir /s /q tmp
 )
 
-REM Adding basebox
+REM Testing newly generated VM into a sandbox
+
 vagrant box remove boot2docker
 vagrant box add boot2docker boot2docker_virtualbox.box
 
 REM Creating tmp VM
-mkdir tmp
+xcopy /S /Y /I tests tmp
 cd tmp
-vagrant init boot2docker
-vagrant up
 
+vagrant up
+if %errorlevel% GEQ 1 (
+	ECHO Could not vagrant up the basebox. See the log for details
+)
+
+REM testing basic vagrant SSH
+@echo off
+vagrant ssh -c 'whoami' -- -T -n
+if %errorlevel% GEQ 1 (
+	ECHO Could not vagrant ssh to the basebox. See the log for details
+)
+
+REM testing vagrant SSH with sudo
+vagrant ssh -c 'sudo whoami' -- -T -n
+if %errorlevel% GEQ 1 (
+	ECHO Could not sudo vagrant ssh to the basebox. See the log for details
+)
+
+REM testing /vagrant mounting
+@echo off
+vagrant ssh -c 'ls -l /vagrant' -- -T -n
+if %errorlevel% GEQ 1 (
+	ECHO Could not access /vagrant into the basebox. See the log for details
+)
+
+REM testing access to bats install script
+@echo off
+vagrant ssh -c "[ $(ls /vagrant | grep install_bats | wc -l) -eq 1 ] || exit 1" -- -T -n
+if %errorlevel% GEQ 1 (
+	ECHO Could not access install_bats on /vagrant into the basebox. See the log for details
+)
+
+REM launching Bats tests inside the VM
+@echo off
+vagrant ssh -c "/bin/bash /vagrant/install_bats.sh" -- -T -n
+vagrant ssh -c "/vagrant/bats/bin/bats --tap /vagrant/*.bats" -- -T -n
+
+
+cd ..
+rmdir /s /q tmp
