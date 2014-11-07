f9009691dc620a277a6934ebd948fc2438438332
Damien DUPORTAL <damien.duportal@gmail.com>
2014-11-07 12:24:21 +0100

    [changelog] Adding the bootlocal tuning possible into the /vagrant share

--- b2d-provision.sh
+++ b2d-provision.sh
@@ -30,3 +30,22 @@ chmod 0600 ${TMP_USERDATA_DIR}/.ssh/authorized_keys
 
 cd ${TMP_USERDATA_DIR}
 tar cf ${B2D_PERSISTENT_DIR}/userdata.tar ./.ssh
+
+
+## I want to re-use the bootlocal.sh stuff, but adding one optionnaly from the /vagrant folder
+cat <<EOF >${B2D_PERSISTENT_DIR}/bootlocal.sh
+# Waiting for /vagrant to be mounted
+while true; do
+    if [[ -d /vagrant ]]; then
+       break
+    fi
+    sleep 1
+done
+sleep 5
+
+# If we have a bootlocal.sh in /vagrant, just run it
+if [[ -x /vagrant/bootlocal.sh ]]; then
+	/bin/sh /vagrant/bootlocal.sh
+fi
+EOF
+sudo chmod a+x ${B2D_PERSISTENT_DIR}/bootlocal.sh
